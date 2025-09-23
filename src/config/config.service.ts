0ccaba225493ba53a69afd4f8d33b72acb6316ac
3x-haust <lyusungyun@icloud.com>
2025-09-23 09:26:28 +0900

    feat: env

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -6,27 +6,43 @@ export class ConfigService {
   constructor(private readonly config: NestConfigService) {}
 
   get port(): number {
-    return Number(this.config.get<number>('PORT', 3000));
+    return Number(this.config.get<number>('PORT_NESTJS', 3000));
+  }
+
+  get prometheusPort(): number {
+    return Number(this.config.get<number>('PORT_PROMETHEUS', 9090));
+  }
+
+  get grafanaPort(): number {
+    return Number(this.config.get<number>('PORT_GRAFANA', 3001));
+  }
+
+  get nodeEnv(): string {
+    return this.config.get<string>('NODE_ENV', 'development');
+  }
+
+  get grafanaAdminPassword(): string {
+    return this.config.get<string>('GRAFANA_ADMIN_PASSWORD', 'admin');
   }
 
   get databaseHost(): string {
-    return this.config.get<string>('DB_HOST', 'localhost');
+    return this.config.get<string>('DATABASE_HOST', 'localhost');
   }
 
   get databasePort(): number {
-    return Number(this.config.get<number>('DB_PORT', 5432));
+    return Number(this.config.get<number>('DATABASE_PORT', 5432));
   }
 
   get databaseUser(): string {
-    return this.config.get<string>('DB_USER', 'postgres');
+    return this.config.get<string>('DATABASE_USER', 'postgres');
   }
 
   get databasePassword(): string {
-    return this.config.get<string>('DB_PASSWORD', 'secret');
+    return this.config.get<string>('DATABASE_PASSWORD', 'postgres');
   }
 
   get databaseName(): string {
-    return this.config.get<string>('DB_NAME', 'joinus');
+    return this.config.get<string>('DATABASE_NAME', 'joinus');
   }
 
   get kafkaBrokers(): string[] {
@@ -41,5 +57,43 @@ export class ConfigService {
 
   get redisPort(): number {
     return Number(this.config.get<number>('REDIS_PORT', 6379));
+  }
+
+  get jwtSecret(): string {
+    return this.config.get<string>('JWT_SECRET', 'your_jwt_secret_here');
+  }
+
+  get adminEmail(): string | undefined {
+    return this.config.get<string>('ADMIN_EMAIL');
+  }
+
+  get adminName(): string | undefined {
+    return this.config.get<string>('ADMIN_NAME');
+  }
+
+  get adminRole(): string | undefined {
+    return this.config.get<string>('ADMIN_ROLE');
+  }
+
+  get adminGrade(): number | undefined {
+    const g = this.config.get<string>('ADMIN_GRADE');
+    return g !== undefined && g !== null ? Number(g) : undefined;
+  }
+
+  get firebaseProjectId(): string | undefined {
+    return this.config.get<string>('FIREBASE_PROJECT_ID');
+  }
+
+  get firebaseClientEmail(): string | undefined {
+    return this.config.get<string>('FIREBASE_CLIENT_EMAIL');
+  }
+
+  get firebasePrivateKey(): string | undefined {
+    const key = this.config.get<string>('FIREBASE_PRIVATE_KEY');
+    return key ? key.replace(/\\n/g, '\n') : undefined;
+  }
+
+  get cookieDomain(): string | undefined {
+    return this.config.get<string>('COOKIE_DOMAIN');
   }
 }
